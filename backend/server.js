f9b9ea535b0a07db9d5be87067df54fcd29f5df5
Jocn2020 <63906312+Jocn2020@users.noreply.github.com>
2021-05-19 14:33:47 -0400

    repair proxy code

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,9 @@ const port = process.env.PORT || 5000;
 
 const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
-    // add other server routes to path array
-    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
-}
+const apiProxy = proxy(['/api' ], { target: 'http://localhost:5000' });
 
+app.use(apiProxy)
 app.use(cors());
 app.use(express.json());
 
